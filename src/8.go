7059319a6a5deeebb90f1149351279830e85dd56
catgenna794 <catgenna794@gmail.com>
2025-03-13 18:47:48 +0000

    Add 8.go

---
+++ src/8.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Declare variables
+	var a int = 4
+	var b int = 7
+
+	// Calculate sum
+	sum := a + b
+
+	// Print result
+	fmt.Println("Sum:", sum)
+}
