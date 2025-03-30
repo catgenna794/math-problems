cc5c1c163bce5dc88241d874b5fd670d367505da
catgenna794 <catgenna794@gmail.com>
2025-03-30 02:01:08 +0000

    Add 20.go

---
+++ src/20.go
@@ -0,0 +1,6 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example Go code to solve a problem in your project math-problems.
+        System.out.println("Hello, go!");
+    }
+}
